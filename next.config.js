073bdbe346715a2f7a7bc4b0ba3b4b98f7d22c36
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-11-12 22:41:17 +0900

    redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/:path",
+        destination:
+          "https://spring-sun-2067.nomadcoders.workers.dev?key=:path",
+        permanent: true,
+      },
+    ];
+  },
+};
